a6102649036573eb09e79232affe267d502c1f40
faqihnafiddin <faqihnafiddin12.com>
2026-07-28 15:26:05 +0000

    Security configuration completed

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,29 @@
-const express=require("express");
-const cors=require("cors");
+import dotenv from "dotenv";
+dotenv.config();
 
-const app=express();
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-res.json({
-message:"Backend Running"
-});
+// Koneksi MongoDB
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("MongoDB Error:", err));
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Backend Running",
+  });
 });
 
-app.listen(5000,()=>{
-console.log("Server Running");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server Running on Port ${PORT}`);
 });
