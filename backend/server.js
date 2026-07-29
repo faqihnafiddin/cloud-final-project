3273dbc386d3910526436caa32af2cd2fe1d756a
faqihnafiddin <faqihnafiddin12.com>
2026-07-29 16:41:14 +0000

    Initial cloud full stack deployment project

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,22 @@
-import dotenv from "dotenv";
-dotenv.config();
+require("dotenv").config();
 
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Koneksi MongoDB
-mongoose
-  .connect(process.env.DB_URL)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error("MongoDB Error:", err));
-
 app.get("/", (req, res) => {
   res.json({
-    message: "Backend Running",
+    message: "Backend Running"
   });
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server Running on Port ${PORT}`);
+  console.log(`Server Running on port ${PORT}`);
 });
